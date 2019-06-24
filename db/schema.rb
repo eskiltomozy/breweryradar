d3a04410d8df0aa0a6539cafaa1502ef9091613f
eskil tomozy <eskil@tomozy.com>
2019-06-24 04:50:22 +0000

    Nomster 3:12 - pagination added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_23_124649) do
+ActiveRecord::Schema.define(version: 2019_06_24_023010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(version: 2019_06_23_124649) do
     t.string "name"
     t.text "description"
     t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
