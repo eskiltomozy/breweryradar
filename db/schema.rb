392ecc0c3a264ae4b86ec43d697c5de5a0e9e0c6
eskil tomozy <eskil@tomozy.com>
2019-06-26 06:04:48 +0000

    Nomster 10:39 - picture uploader added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_25_013312) do
+ActiveRecord::Schema.define(version: 2019_06_25_153552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,17 @@ ActiveRecord::Schema.define(version: 2019_06_25_013312) do
     t.index ["user_id", "place_id"], name: "index_comments_on_user_id_and_place_id"
   end
 
+  create_table "photos", force: :cascade do |t|
+    t.text "caption"
+    t.integer "user_id"
+    t.integer "place_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "picture"
+    t.index ["place_id"], name: "index_photos_on_place_id"
+    t.index ["user_id", "place_id"], name: "index_photos_on_user_id_and_place_id"
+  end
+
   create_table "places", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -36,11 +47,6 @@ ActiveRecord::Schema.define(version: 2019_06_25_013312) do
     t.float "latitude"
     t.float "longitude"
     t.index ["user_id"], name: "index_places_on_user_id"
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
