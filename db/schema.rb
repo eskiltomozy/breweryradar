12f05b7d3f5e94757873c1ab9d6e347903b089aa
eskil tomozy <eskil@tomozy.com>
2019-06-27 05:16:57 +0000

    Nomster 10:39 - db:reset

--- db/schema.rb
+++ db/schema.rb
@@ -29,17 +29,6 @@ ActiveRecord::Schema.define(version: 2019_06_26_101003) do
     t.index ["user_id", "place_id"], name: "index_comments_on_user_id_and_place_id"
   end
 
-  create_table "photos", force: :cascade do |t|
-    t.text "caption"
-    t.integer "user_id"
-    t.integer "place_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "picture"
-    t.index ["place_id"], name: "index_photos_on_place_id"
-    t.index ["user_id", "place_id"], name: "index_photos_on_user_id_and_place_id"
-  end
-
   create_table "places", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -53,13 +42,13 @@ ActiveRecord::Schema.define(version: 2019_06_26_101003) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
