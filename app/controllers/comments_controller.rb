8f21f4f4bb918d74296a0dabb73ca5a08aee3c17
eskil tomozy <eskil@tomozy.com>
2019-06-29 08:40:03 +0000

    Nomster 13:51 profile user breweries added

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,41 @@
 class CommentsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:create, :edit, :update, :destroy]
 
   def create
     @place = Place.find(params[:place_id])
     @place.comments.create(comment_params.merge(user: current_user))
     redirect_to place_path(@place)
+  end
+
+  def edit
+    @place = Place.find(params[:place_id])
+    @comment = Comment.find(params[:place_id])
+    if @place.user != current_user
+      return render plain: 'Not allowed', status: :forbidden
+    end
+  end
+  
+  def update
+    @place = Place.find(params[:place_id])
+    @comment = Comment.find(params[:place_id])
+    if @comment.user != current_user
+      return render plain: 'Not allowed', status: :forbidden
+    end
+    @comment.update_attributes(comment_params)
+    if @comment.valid?
+      redirect_to user_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:place_id])
+    if @comment.user != current_user
+      return render plain: 'Not allowed', status: :forbidden
+    end
+    @comment.destroy
+    redirect_to user_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'places#index'
   resources :places do
-    resources :comments, :photos, only: :create
+    resources :comments, only: [:create, :edit, :update, :destroy]
   end
   resources :users, only: :show
 end
